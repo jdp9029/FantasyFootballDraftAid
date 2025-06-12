3a0bcb275a4f432921e1b94f3db30c31bbc088c0
jdp9029 <jdp9029@g.rit.edu>
2025-06-12 13:49:52 -0400

    bad commit msgs are bad

--- Assets/Scripts/Draft Screen/ActiveDraftStats.cs
+++ Assets/Scripts/Draft Screen/ActiveDraftStats.cs
@@ -16,7 +16,7 @@ public class ActiveDraftStats : MonoBehaviour
     // Start is called before the first frame update
     void Start() 
     {
-        SceneManager.LoadScene("Draft Screen");
+        //SceneManager.LoadScene("Draft Screen");
     }
 
     // Update is called once per frame
